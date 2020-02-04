87799cf6eb956200c436bdc1e50737b3cfca47fb
johannesmayer <johannes.mayer@ptb.de>
2020-02-04 16:05:35 +0000

    supplied cartesian trajectory preparation

--- src/xGadgetron/cGadgetron/encoding.cpp
+++ src/xGadgetron/cGadgetron/encoding.cpp
@@ -1,2 +1,35 @@
 #include "sirf/Gadgetron/encoding.h"
 
+
+
+#include <sstream>
+
+#include "sirf/iUtilities/LocalisedException.h"
+
+
+using namespace sirf;
+using namespace ISMRMRD;
+
+void sirf::aTrajectoryPreparation::update_acquisitions_info(MRAcquisitionData& mr_acq)
+{
+
+    IsmrmrdHeader hdr = mr_acq.acquisitions_info().get_IsmrmrdHeader();
+
+    if(hdr.encoding.size() > 0)
+        throw LocalisedException("Currrently only files with one encoding are supported", __FILE__, __LINE__);
+
+    hdr.encoding[0].trajectory = this->traj_type_;
+
+    this->kspace_encoding_ = hdr.encoding[0];
+
+    std::stringstream info_stream;
+    serialize(hdr, info_stream);
+    mr_acq.set_acquisitions_info(info_stream.str());
+
+}
+
+void sirf::CartesianTrajectoryPrep::set_trajectory(MRAcquisitionData& mr_acq)
+{
+    update_acquisitions_info(mr_acq); // do nothing for cartesian trajectories
+}
+
--- src/xGadgetron/cGadgetron/include/sirf/Gadgetron/encoding.h
+++ src/xGadgetron/cGadgetron/include/sirf/Gadgetron/encoding.h
@@ -6,40 +6,39 @@
 #include <ismrmrd/xml.h>
 #include "sirf/Gadgetron/gadgetron_data_containers.h"
 
-class aTrajectoryPreparation
-{
+namespace sirf{
+
+class aTrajectoryPreparation{
+
 public:
-    aTrajectoryPreparation(){};
+    aTrajectoryPreparation();
     virtual void set_trajectory(sirf::MRAcquisitionData& mr_acq)=0;
 protected:
 
-    ISMRMRD::EncodingLimits enc_lims_;
+    virtual void update_acquisitions_info(sirf::MRAcquisitionData& mr_acq)=0;
+
+    ISMRMRD::Encoding kspace_encoding_;
     ISMRMRD::TrajectoryType traj_type_;
     int traj_dim_;
-    virtual std::vector<float> compute_trajectory(ISMRMRD::Acquisition& acq);
+    virtual std::vector<float> compute_trajectory(ISMRMRD::Acquisition& acq)=0;
+
+};
+
+
+
+class CartesianTrajectoryPrep : public aTrajectoryPreparation{
+
+public:
+    CartesianTrajectoryPrep(): aTrajectoryPreparation() {
+        traj_type_ = ISMRMRD::TrajectoryType::CARTESIAN;
+        traj_dim_ = 0;
+    }
+    virtual void set_trajectory(sirf::MRAcquisitionData& mr_acq);
 
 };
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+} // end namespace
 #endif // ENCODING_H
